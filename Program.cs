2f58c058381fe789441645d717905ee951bbaa44
vladpotuk <vladpotuk01@gmail.com>
2024-02-23 20:01:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть речення:");
+        string sentence = Console.ReadLine();
+
+        string reversedSentence = ReverseWords(sentence);
+
+        Console.WriteLine($"Після перевороту: {reversedSentence}");
+
+        Console.ReadLine();
+    }
+
+    static string ReverseWords(string sentence)
+    {
+        
+        string[] words = sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        
+        for (int i = 0; i < words.Length; i++)
+        {
+            words[i] = ReverseWord(words[i]);
+        }
+
+        
+        string reversedSentence = string.Join(" ", words);
+        return reversedSentence;
+    }
+
+    static string ReverseWord(string word)
+    {
+        
+        char[] charArray = word.ToCharArray();
+
+        
+        Array.Reverse(charArray);
+
+        
+        return new string(charArray);
+    }
+}
+
